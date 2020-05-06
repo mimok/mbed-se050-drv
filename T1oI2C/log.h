0b088f4ec2721c2907adc77f05a1056c757d2c71
Michael Grand <michael.grand.mg@gmail.com>
2020-05-06 18:57:10 +0200

    MBED_CONF_SE050_LOGEN macro added to disable logging
    T1oI2C logging can be disabled using the MBED_CONF_SE050_LOGEN macro.
    This macro can be set using the corresponding variable in mbed_lib.json
    file or by overriding it in mbed_app.json.
    
    Signed-off-by: Michael Grand <michael.grand.mg@gmail.com>

--- T1oI2C/log.h
+++ T1oI2C/log.h
@@ -21,9 +21,15 @@
 #include "platform/mbed_debug.h"
 #include "platform/mbed_error.h"
 
+#if MBED_CONF_SE050_LOGEN
 #define LOG_E(...) printf("Fatal Error: "); error(__VA_ARGS__); printf("\n")
 #define LOG_D(...) debug(__VA_ARGS__); printf("\n")
 #define LOG_W(...) debug(__VA_ARGS__); printf("\n")
+#else
+#define LOG_E(...)
+#define LOG_D(...)
+#define LOG_W(...)
+#endif
 
 #define ENSURE_OR_GO_EXIT(test) if(!(test)) goto exit;
 
